f81de3c504f39366018b62cae49d32d72cc0a7d2
annapgsm <anna.pingsmann@gmail.com>
2025-10-10 16:13:38 +0200

    Updated index.jsx
    imported MainView

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,14 @@
 import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
 
 import "./index.scss";
 
 const MyFlixApplication = () => {
   return (
-    <div className="my-flix">
-      <div>Good morning</div>
-    </div>
+    <MainView />
   );
 };
 
 const container = document.querySelector("#root");
 const root = createRoot(container);
-
 root.render(<MyFlixApplication />);

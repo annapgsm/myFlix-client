836b54e3971ca71edf4569501036840bf1f89be5
annapgsm <anna.pingsmann@gmail.com>
2025-10-24 17:32:21 +0200

    Updated movie-card.jsx
    -Imported Button, Card and Link
    -Added "Add to favorites" function

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie, onMovieClick, onAddFavorite,favoriteMovies = []}) => {
+    const isFavorite = favoriteMovies.includes(movie._id); // check if it's already favorite
     return (
-        <div
-          onClick={()=>{                //callback function
-              onMovieClick(movie);
-          }}
-        >
-            {movie.Title}
-        </div>
+        <Card>
+            <Card.Img variant="top" src={movie.ImagePath} />
+            <Card.Body>
+                <Card.Title>{movie.Title}</Card.Title>
+                <Card.Text>Directed by: {movie.Director?.Name}</Card.Text>
+                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                <Button variant="link">Open</Button>
+                </Link>
+
+                {onAddFavorite && (
+                <Button
+                    variant={isFavorite ? "success" : "outline-primary"} // green if favorite, blue outline if not
+                    size="sm"
+                    className="mt-2"
+                    onClick={() => onAddFavorite(movie._id)}
+                >
+                    {isFavorite ? "Added to Favorites" : "Add to Favorites"}
+                </Button>
+                )}
+            </Card.Body>
+        </Card>
     );
 };
 
@@ -18,18 +35,13 @@ MovieCard.propTypes = {
     movie: PropTypes.shape({
         _id: PropTypes.string.isRequired,
         Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        Genre: PropTypes.shape({
-            Name: PropTypes.string,
-            Description: PropTypes.string
-        }),
         Director: PropTypes.shape({
-            Name: PropTypes.string,
-            Bio: PropTypes.string
+            Name: PropTypes.string
         }),
-        ImagePath: PropTypes.string,
+        ReleaseYear: PropTypes.number,
+        ImagePath: PropTypes.string.isRequired,
         Featured: PropTypes.bool
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    onAddFavorite: PropTypes.func,
 };
 

bb5d425ce6550bc7262699f8dfbe4de68a4baa17
annapgsm <anna.pingsmann@gmail.com>
2026-04-14 13:18:16 +0200

    fix: restore movie filtering after moving search to navbar

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -5,26 +5,32 @@ import { MovieCard } from "../movie-card/movie-card";
 import "./movies-list.scss";
 
 export const MoviesList = ({ favoriteMovies, onAddFavorite }) => {
-  const movies = useSelector((state) => state.movies.movies.list) || [];
-  const filter = (useSelector((state) => state.movies.movies.filter) || "").trim().toLowerCase();
+    const movies = useSelector((state) => state.movies.movies.list) || [];
+    const filter = (useSelector((state) => state.movies.movies.filter) || "").trim().toLowerCase();
   
-  return (
-    <>
-        <Row>
-            {movies.length === 0 ? (
-                <Col>The list is empty!</Col>
-            ) : (
-                filteredMovies.map((movie) => (
-                    <Col className="mb-4 mt-2" key={movie._id} xs={12} sm={6}md={3}>
-                        <MovieCard 
-                        movie={movie}
-                        onAddFavorite={onAddFavorite}
-                        favoriteMovies={favoriteMovies || []} />
-                    </Col>
-                ))
-            )}
-        </Row>
-    </>
-  );
+    const filteredMovies = movies.filter(
+        (movie) =>
+        !filter ||
+        (movie.Title && movie.Title.toLowerCase().includes(filter))
+    );
+
+    return (
+        <>
+            <Row>
+                {movies.length === 0 ? (
+                    <Col>The list is empty!</Col>
+                ) : (
+                    filteredMovies.map((movie) => (
+                        <Col className="mb-4 mt-2" key={movie._id} xs={12} sm={6}md={3}>
+                            <MovieCard 
+                            movie={movie}
+                            onAddFavorite={onAddFavorite}
+                            favoriteMovies={favoriteMovies || []} />
+                        </Col>
+                    ))
+                )}
+            </Row>
+        </>
+    );
 };
 
